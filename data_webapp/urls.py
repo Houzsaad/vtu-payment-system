0b0d7c4a9534f702df91b978ea71659f1250f00a
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-04-10 15:41:42 -0900

    fix internal server error

--- data_webapp/urls.py
+++ data_webapp/urls.py
@@ -6,17 +6,12 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 
-<<<<<<< HEAD
     path('', include('accounts.urls')),
-
-    #path('', home, name='home'),
     
-=======
     path('', home, name='home'),
 
     path('accounts/', include('accounts.urls')),
 
->>>>>>> fe80a850a364d529771be31eab55158a9c160e92
     path('services/', include('services.urls')),
 
     path('notifications/', include('notifications.urls')),

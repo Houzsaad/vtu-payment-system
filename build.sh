02f62e83519277a87b7fe3b46484ea9be907a523
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-04-12 13:57:21 -0900

    add superuser creation to build

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c"
+from accounts.models import Costomer
+if not Costomer.objects.filter(emial='admin@admin.com').exists():
+    Costomer.objects.create_superuser('smahadi@admin.com', '09065379803', 'smahadi2026')

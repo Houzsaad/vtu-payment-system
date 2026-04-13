b19bf2120029cf149037c11c3ca45691eecf6a81
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-04-13 00:44:39 -0900

    load fixtures on deploy

--- build.sh
+++ build.sh
@@ -5,11 +5,17 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
+python manage.py dumpdata services --indent 2 > services/fixtures/initial_data.json
 python manage.py shell -c"
-
+import os
 from accounts.models import Costomer
+email = os.environ.get('ADMIN_EMAIL')
+phone_number = os.environ.get('ADMIN_PHONE')
+password = os.environ.get('ADMIN_PASSWORD')
 
-if not Costomer.objects.filter(email='smahadi@admin.com').exists():
-    Costomer.objects.create_superuser('smahadi@admin.com', '09065379803', 'smahadi0012026')
+if not Costomer.objects.filter(email=email).exists():
+    Costomer.objects.create_superuser(email, phone_number, password)
+    user.is_staff = True
+    user.is_superuser = True
+    user.save()
 "

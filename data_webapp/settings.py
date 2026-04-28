45aab8143b93e17a5afbef7a0da9cbac0d53e6fd
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-04-28 02:37:30 -0900

    Edited profile site it's more professional so far

--- data_webapp/settings.py
+++ data_webapp/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='django-secre-key(@Mam@2023.@#$%#@$%67742003****)' #'django-insecure-&4^vbo-cdi=2+5_1pte65r0n50scyxp4r573&kbp+x3qt@h#'
 
 
 DODODATA_API_KEY = os.environ.get("DODODATA_API_KEY")
